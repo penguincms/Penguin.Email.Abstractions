7b74f5eb20e1751d923738a40fc23b628065a438
anloffredo <anloffredo@massageenvy.com>
2020-05-14 19:34:47 -0700

    Auto Commit

--- Interfaces/IEmailHandler.cs
+++ Interfaces/IEmailHandler.cs
@@ -3,6 +3,7 @@
     /// <summary>
     /// Used to identify that a class contains methods that generate emails based on templates
     /// </summary>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1040:Avoid empty interfaces", Justification = "<Pending>")]
     public interface IEmailHandler
     {
     }

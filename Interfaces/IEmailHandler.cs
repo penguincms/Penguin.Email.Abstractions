f1622a8d263a7773e304c9b8eecc3c4bf4fa6099
AnLoffredo <38223101+AnLoffredo@users.noreply.github.com>
2021-07-08 14:49:36 -0700

    Autocommit
    Autocommit

--- Interfaces/IEmailHandler.cs
+++ Interfaces/IEmailHandler.cs
@@ -3,7 +3,6 @@
     /// <summary>
     /// Used to identify that a class contains methods that generate emails based on templates
     /// </summary>
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1040:Avoid empty interfaces", Justification = "<Pending>")]
     public interface IEmailHandler
     {
     }
